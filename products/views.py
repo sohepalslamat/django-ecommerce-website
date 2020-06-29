b0786e3acedd29825ce22c5c126b7aafae2f7b80
sohep alslamat <sohep.alslamat91@gmail.com>
2020-06-29 22:13:53 +0300

    add products app and some views

---
+++ products/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.utils import timezone
+
+
+def say_hi(request, name):
+    return render(request, "say_hi.html", {"name": name})
+
+
+def show_time(request):
+    now = timezone.now()
+    return render(request, "show_time.html", {"now": now})
