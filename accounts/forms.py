4283f93c60de6f7147d3f7bf1d2a291dd177ef24
sohep alslamat <sohep.alslamat91@gmail.com>
2020-07-13 23:49:56 +0300

    add address field to user model

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.db import models
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+User.add_to_class('address', models.CharField(
+    max_length=75, blank=True, null=True))
 
 
 class signUpForm(UserCreationForm):
@@ -12,8 +15,10 @@ class signUpForm(UserCreationForm):
         max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(
         max_length=254, help_text='Required Inform a valid email address.')
+    address = forms.CharField(
+        max_length=75)
 
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'password1', 'password2',
-                  'email')
+                  'email', 'address')
