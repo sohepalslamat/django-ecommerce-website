741518a1f76c380edd851c2b5ecf86ab2dde762a
sohep alslamat <sohep.alslamat91@gmail.com>
2020-07-16 22:14:09 +0300

    password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,13 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(), name="login"),
     path('logout', auth_views.LogoutView.as_view(), name="logout"),
     path('activate/<int:uid>/<token>/', activate_email, name="activate_email"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name="password_reset_done"),
+    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name="password_reset_confirm"),
+    path('password-reset/completee/', auth_views.PasswordResetCompleteView.as_view(),
+         name="password_reset_complete"),
 
 ]
