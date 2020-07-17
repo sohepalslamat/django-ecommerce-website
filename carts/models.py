40a8d6cdad02cc5b7db672b6a13de7d082effba0
sohep alslamat <sohep.alslamat91@gmail.com>
2020-07-17 21:25:51 +0300

    create_user_cart when create user

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from products.models import Product
 User = get_user_model()
@@ -12,4 +14,10 @@ class Cart(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_user_cart(sender, instance, created, ** kwargs):
+    if created:
+        Cart.objects.create(user=instance)
