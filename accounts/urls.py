ab30dbd1cc23b83d15da7e94323e3ed61a53a116
sohep alslamat <sohep.alslamat91@gmail.com>
2020-07-19 22:45:17 +0300

    fix url in login

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ from .views import signup, activate_email
 
 urlpatterns = [
     path('signup', signup, name="signup"),
-    path('login', auth_views.LoginView.as_view(), name="login"),
-    path('logout', auth_views.LogoutView.as_view(), name="logout"),
+    path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('activate/<int:uid>/<token>/', activate_email, name="activate_email"),
     path('password_reset/', auth_views.PasswordResetView.as_view(),
          name="password_reset"),
