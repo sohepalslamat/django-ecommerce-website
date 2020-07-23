8e3e891a41b7dd20ea0afc2f1b76279cbc1b96ae
sohep alslamat <sohep.alslamat91@gmail.com>
2020-07-23 21:19:32 +0300

    fix host

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -4,9 +4,9 @@ from .settings import *
 
 SECRET_KEY = environ.get('SECRET_KEY')
 
-ALLOWED_HOSTS = ['https://store-buy.herokuapp.com']
+ALLOWED_HOSTS = ['store-buy.herokuapp.com']
 
-DEBUG = True
+DEBUG = False
 
 
 DATABASES = {
