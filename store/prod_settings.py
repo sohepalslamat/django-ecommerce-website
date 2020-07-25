1f60e7e6b8b590d58102d7e24b5b2386c21d24e6
sohep alslamat <sohep.alslamat91@gmail.com>
2020-07-25 19:44:24 +0300

    add email conf

--- store/prod_settings.py
+++ store/prod_settings.py
@@ -1,4 +1,4 @@
-from os import environ
+from os import environ, getenv
 import dj_database_url
 from .settings import *
 
@@ -6,7 +6,11 @@ SECRET_KEY = environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['store-buy.herokuapp.com']
 
-DEBUG = False
+debug_status = environ.get('DEBUG').lower()
+if debug_status == 'true':
+    DEBUG = True
+else:
+    DEBUG = False
 
 
 DATABASES = {
@@ -14,3 +18,15 @@ DATABASES = {
         default=environ.get('DATABASE_URL')
     )
 }
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL ')
+
+SENDGRID_API_KEY = getenv('SENDGRID_API_KEY')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
