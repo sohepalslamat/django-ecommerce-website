33a8c19d06aa219dc66f5b03cf7efd135920f532
sohep alslamat <sohep.alslamat91@gmail.com>
2020-08-14 22:49:16 +0300

    style product-details

--- products/urls.py
+++ products/urls.py
@@ -3,8 +3,8 @@ from .views import products_list, product_details, product_add, product_edit, pr
 
 urlpatterns = [
     path('', products_list, name="products_list"),
-    path('add', product_add, name="product_add"),
-    path('<int:pk>', product_details, name="product_details"),
-    path('edit/<int:pk>', product_edit, name="product_edit"),
-    path('delete/<int:pk>', product_delete, name='product_delete'),
+    path('add/', product_add, name="product_add"),
+    path('<int:pk>/', product_details, name="product_details"),
+    path('edit/<int:pk>/', product_edit, name="product_edit"),
+    path('delete/<int:pk>/', product_delete, name='product_delete'),
 ]
